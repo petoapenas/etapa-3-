7969e7e856ae8a807d2505266c1c30b44dbf2f2c
Petoteta <matheusmicheline7@gmail.com>
2023-04-04 19:29:36 -0300

    Exercicio Audio Midi

---
+++ aluramidi-curso-aula3/main.js
@@ -0,0 +1,29 @@
+function tocaSom (idElementAudio) {
+
+    document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0; 
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string 
+    const idAudio = `#som_${instrumento}`
+    console.log(idAudio)
+
+    
+    
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    
+    contador = contador + 1; 
+    
+    console.log(contador);
+}
