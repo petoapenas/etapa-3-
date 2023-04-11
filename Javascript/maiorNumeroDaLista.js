7614c2fef7c822748f184889453f6b1631b33758
Thiago Cerutti <thiagocerutti@gmail.com>
2023-04-11 19:56:11 -0300

    implementado busca de maior número

---
+++ Javascript/maiorNumeroDaLista.js
@@ -0,0 +1,14 @@
+function buscaMaiorValor(listaDeNumeros) {
+  let maiorNumero = listaDeNumeros[0];
+
+  for (let i = 0; i < listaDeNumeros.length; i++) {
+    if (listaDeNumeros[i] > maiorNumero) {
+      maiorNumero = listaDeNumeros[i];
+    }
+  }
+  return maiorNumero;
+}
+
+const lista = [1, 2, 3, 4, 5, 3, 8, 3];
+const maior = buscaMaiorValor(lista);
+console.log(`maior número é ${maior}`);
